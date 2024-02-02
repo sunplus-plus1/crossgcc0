f4b5b47a99feeb2ed91ce1da59de0e3a3adfe2f8
jacky.hsieh <jacky.hsieh@sunplus.com>
2024-02-02 10:31:56 +0800

    remove riscv64

--- riscv64-sifive-linux-gnu/lib/gcc/riscv64-sifive-linux-gnu/8.3.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "./configure --prefix=/home/qinjian/freedom-u-sdk/work/buildroot_initramfs/host --sysconfdir=/home/qinjian/freedom-u-sdk/work/buildroot_initramfs/host/etc --enable-static --target=riscv64-sifive-linux-gnu --with-sysroot=/home/qinjian/freedom-u-sdk/work/buildroot_initramfs/host/riscv64-sifive-linux-gnu/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/home/qinjian/freedom-u-sdk/work/buildroot_initramfs/host --with-mpc=/home/qinjian/freedom-u-sdk/work/buildroot_initramfs/host --with-mpfr=/home/qinjian/freedom-u-sdk/work/buildroot_initramfs/host --with-pkgversion='Buildroot 2019.02-07449-g4eddd28' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --enable-tls --disable-libmudflap --enable-threads --without-isl --without-cloog --with-arch=rv64imafd --with-abi=lp64d --enable-languages=c,c++ --with-build-time-tools=/home/qinjian/freedom-u-sdk/work/buildroot_initramfs/host/riscv64-sifive-linux-gnu/bin --enable-shared --disable-libgomp";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafd" } };
--- riscv64-sifive-linux-gnu/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/qinjian/freedom-u-sdk/work/buildroot_initramfs/host/lib"
-XML2_LIBS="-lxml2      -lm "
-XML2_INCLUDEDIR="-I/home/qinjian/freedom-u-sdk/work/buildroot_initramfs/host/include/libxml2"
-MODULE_VERSION="xml2-2.9.9"
-
